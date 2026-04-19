f1a50534cac5b1aa73a7f8367d4a21f62a4826f5
highjeans <vivan.waghela23@gmail.com>
2026-04-19 21:56:06 +0100

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} h-full antialiased`}>
       <body className="min-h-full bg-slate-50 font-sans dark:bg-slate-950">
         {children}
+        <Analytics />
       </body>
     </html>
   );
